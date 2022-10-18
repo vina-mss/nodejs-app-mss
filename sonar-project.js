c79ae7e34508cae54841f5e221a64961223af71e
vinayagsivan <112825237+vinayagsivan@users.noreply.github.com>
2022-10-18 10:45:35 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,15 +1,15 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'https://sonarcloud.io',
-  //serverUrl: 'http://172.31.34.218:9000/',
+  //serverUrl: 'https://sonarcloud.io',
+  serverUrl: 'http://172.31.34.218:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    'sonar.organization':'sonarcloud-vina',
-	    'sonar.login': 'b9130c51a02c0aa7b5e9739b628492bd06f135b4', 
-	    //'sonar.login': '9bcdd15299583c728cf53837f2ab21fcb36c5aa4',
+	    'sonar.login': 'squ_c9731897d5192207b1195cfdc5ed5e9c76647f6e',
+	    // SonarCloudToken 'sonar.login': 'b9130c51a02c0aa7b5e9739b628492bd06f135b4', 
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
